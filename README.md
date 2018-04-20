fafc6a29cb5ab59659db53d6f18093e137413bfc
freewind <nowindlee@gmail.com>
2018-04-20 13:51:01 +0800

    secret key be same

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-Go Hello World Demo
-===================
+nacl/box Shared Key Demo
+=========================
+
+For two pairs of private/public key, the sharedSecret key between them are the same.
+
+So they can just send the public key to peer and they know how to Seal/Open messages.
 
 ```
 brew install go
+go get
 go run hello.go
 ```
 
-You will see `Hello, go!`
--- hello.go
+++ hello.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"golang.org/x/crypto/nacl/box"
+	"crypto/rand"
+	"github.com/golang-demos/go-utils-demo/goutils"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello go!")
+	pubKey1, priKey1, _ := box.GenerateKey(rand.Reader)
+	pubKey2, priKey2, _ := box.GenerateKey(rand.Reader)
+
+	var sharedKey1 [32]byte
+	box.Precompute(&sharedKey1, pubKey2, priKey1)
+
+	var sharedKey2 [32]byte
+	box.Precompute(&sharedKey2, pubKey1, priKey2)
+
+	fmt.Println("shared key1 and key2 should be the same")
+	goutils.PrintlnByteArray("shared key1", sharedKey1[:])
+	goutils.PrintlnByteArray("shared key2", sharedKey2[:])
 }
